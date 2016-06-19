e2c5441f343388dd37c51f7fe60c10e9d559fb0a
Timothy Sanders <tsanders@hiddenjester.com>
2016-06-18 18:41:42 -0700

    Adding iOS target (for WatchOS 1 extensions)

---
+++ WatchColorsiOS/WatchColorsiOS.h
@@ -0,0 +1,19 @@
+//
+//  WatchColorsiOS.h
+//  WatchColorsiOS
+//
+//  Created by Timothy Sanders on 6/18/16.
+//  Copyright © 2016 Timothy Sanders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatchColorsiOS.
+FOUNDATION_EXPORT double WatchColorsiOSVersionNumber;
+
+//! Project version string for WatchColorsiOS.
+FOUNDATION_EXPORT const unsigned char WatchColorsiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchColorsiOS/PublicHeader.h>
+
+
