1787c4737a027e7cbf9354d1d913435d62862af6
Timothy Sanders <tsanders@hiddenjester.com>
2016-06-18 18:32:11 -0700

    Setting up framework for Carthage

---
+++ WatchColors/WatchColors.h
@@ -0,0 +1,19 @@
+//
+//  WatchColors.h
+//  WatchColors
+//
+//  Created by Timothy Sanders on 6/18/16.
+//  Copyright © 2016 Timothy Sanders. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for WatchColors.
+FOUNDATION_EXPORT double WatchColorsVersionNumber;
+
+//! Project version string for WatchColors.
+FOUNDATION_EXPORT const unsigned char WatchColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchColors/PublicHeader.h>
+
+
